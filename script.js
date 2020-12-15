bb14da53fd96cbb5bbab316c1a575e3a07959f6f
jullmi <julylawerid@gmail.com>
2020-12-15 18:55:58 +0300

    added files and corrected index

---
+++ script.js
@@ -0,0 +1,29 @@
+// // index
+//
+// const textareaAll = document.querySelectorAll("h3")
+//
+//
+// const textarea1 = textareaAll [5]
+// const textarea2 = textareaAll [6]
+// const textarea3 = textareaAll [7]
+//
+// setTimeout(()=> {
+//     hello(textarea1, 'Как выращиваю комнатные растения из листочков')
+// }, 500)
+//
+// setTimeout(()=> {
+//     hello(textarea2, 'Как выращиваю комнатные растения из листочков')
+// }, 500)
+//
+// setTimeout(()=> {
+//     hello(textarea3, 'Как выращиваю комнатные растения из листочков')
+// }, 500)
+//
+//
+// function hello (node, text) {
+//     node.textContent = text
+// }
+//
+// console.log(textarea1)
+// console.log(textarea2)
+// console.log(textarea3)
